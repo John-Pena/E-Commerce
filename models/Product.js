a1f1880291fda8c8c4d75c4fdaa36c6b2892cd3b
John-Pena <jmp1495@gmail.com>
2022-05-06 13:45:00 -0500

    added validation to Product Model

--- models/Product.js
+++ models/Product.js
@@ -9,25 +9,35 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // define id column
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true
     },
+    // define product_name column
     product_name: {
       type: DataTypes.STRING,
       allowNull: false
     },
+    // define price column
     price: {
-      type: DataTypes.INTEGER,
-      allowNull: false
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
     },
+    // define stock column
     stock: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        isNumeric: true
+      }
     },
+    // define category_id column
     category_id: {
       type: DataTypes.INTEGER,
       references: {
